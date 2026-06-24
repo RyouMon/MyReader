edf856549cf29c2a4273d1fc95450ac99c9557c7
Wen Liang <wenslife@outlook.com>
2026-06-24 18:11:46 +0800

    chore(mobile): remove dead reader and download UI

--- my-reader-mobile/src/features/reader/components/reader/chrome/index.ts
+++ my-reader-mobile/src/features/reader/components/reader/chrome/index.ts
@@ -1,9 +1,6 @@
 export { ReaderChapterLabel } from "./ReaderChapterLabel";
 export type { ReaderChapterLabelProps } from "./ReaderChapterLabel";
 
-export { ReaderPageLabel } from "./ReaderPageLabel";
-export type { ReaderPageLabelProps } from "./ReaderPageLabel";
-
 export { default as ReaderCloseButton } from "./ReaderCloseButton";
 
 export { default as ReaderMoreButton } from "./ReaderMoreButton";
@@ -16,16 +13,7 @@ export type { ReaderTocSheetProps } from "./ReaderTocSheet";
 export { default as ReaderSettingsSheet } from "./ReaderSettingsSheet";
 export type { ReaderSettingsSheetProps } from "./ReaderSettingsSheet";
 
-export {
-  SettingSectionLabel,
-  SettingSegment,
-  SettingThemeCard,
-  SettingStepper,
-  ThemeSwatches,
-  FontPicker,
-  SliderControl,
-  StepperControl,
-} from "./SettingControls";
+export { ThemeSwatches, FontPicker, SliderControl, SegmentPicker } from "./SettingControls";
 
 export { READER_THEME_OPTIONS } from "./readerChromeConstants";
 export type { ThemeOption } from "./readerChromeConstants";
